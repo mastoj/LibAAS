89e444761a74469d4eea09c0daf4a51e5135b442
Leif Battermann <leif.battermann@arcor.de>
2016-04-22 11:32:47 +0200

    ex3/done made cmd handlers explicit

--- ex3/done/LibAAS.Contracts/Commands.fs
+++ ex3/done/LibAAS.Contracts/Commands.fs
@@ -2,9 +2,22 @@
 module LibAAS.Contracts.Commands
 
 type CommandData = 
-    | LoanItem of LoanId * UserId * ItemId * LibraryId
-    | ReturnItem
-    | RegisterInventoryItem of Item * Quantity
+    | LoanItem of LoanItem
+    | ReturnItem of ReturnItem
+    | RegisterInventoryItem of RegisterInventoryItem
+
+and LoanItem = {
+    Id:LoanId
+    UserId:UserId
+    ItemId:ItemId
+    LibraryId:LibraryId }
+
+and ReturnItem = {
+    Id:LoanId }
+
+and RegisterInventoryItem = {
+    Item:Item
+    Quantity:Quantity }
 
 type Command = AggregateId * CommandData
 
