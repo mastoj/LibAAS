1a801f3fad1ef39f648e5305bf511ba7255fb92a
nikolaia <nikolaia@users.noreply.github.com>
2016-06-16 16:46:18 +0200

    Add missing fake targets

--- build.fsx
+++ build.fsx
@@ -7,7 +7,7 @@ let testDir = ".test"
 
 let targetName = getBuildParam "target"
 trace (sprintf "Target name: %s" targetName)
-let targets = ["Ex1Start";"Ex1Done";"Ex3Start";"Ex2Done";"Ex3Start";"Ex3Done";"Ex4Start"] |> List.map (fun s -> s.ToLower())
+let targets = ["Ex1Start";"Ex1Done";"Ex2Start";"Ex2Done";"Ex3Start";"Ex3Done";"Ex4Start";"Ex4Start"] |> List.map (fun s -> s.ToLower())
 
 if targets |> List.contains (targetName.ToLower()) |> not then
     let targetNames = String.Join("|", targets)
@@ -17,34 +17,15 @@ if targets |> List.contains (targetName.ToLower()) |> not then
 let (proj,version) = (targetName.Substring(0,3), targetName.Substring(3))
 let basePath = sprintf "./%s/%s" proj version
 
-let createBuildAndTest proj version =
-  let basePath = sprintf "./%s/%s" proj version
-  let build() =
-      trace (sprintf "Building %s %s" proj version)
-      let sln = !! (basePath </> "*.sln")
-      trace (sprintf "Will build solution: %A" sln)
-      sln
-      |> MSBuildRelease "" "Rebuild"
-      |> ignore
-
-  let test() =
-    let testDlls = !!(basePath </> "*.Tests/bin/Debug/*.Tests.dll")
-    testDlls
-    |> xUnit2 (fun p -> { p with HtmlOutputPath = Some (testDir @@ "xunit.html") })
-
-  let restorePackages() =
-    basePath </> "**/packages.config"
-    |> RestorePackage (fun p ->
-        { p with
-            Sources = p.Sources
-            OutputPath = (basePath </> "packages")
-            Retries = 4 })
-    |> ignore
-
-  fun _ ->
-    restorePackages()
-    build()
-    test()
+// Add targets so they are found by the Ionide FAKE plugin
+Target "Ex1Start" ignore
+Target "Ex1Done" ignore
+Target "Ex2Start" ignore
+Target "Ex2Done" ignore
+Target "Ex3Start" ignore
+Target "Ex3Done" ignore
+Target "Ex4Start" ignore
+Target "Ex4Done" ignore
 
 Target "Default" (fun _ ->
   trace "Hello default"
